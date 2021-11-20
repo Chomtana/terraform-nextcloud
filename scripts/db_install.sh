3f8a14190b55236cee0d64b0a6ab1769797aefc9
Chomtana <Chomtana001@gmail.com>
2021-11-20 23:36:23 +0700

    Successful deploy

--- scripts/db_install.sh
+++ scripts/db_install.sh
@@ -2,19 +2,15 @@
 apt update -y
 apt install -y mariadb-server
 
-echo "
-[mysqld]
+echo "[mysqld]
 skip-networking=0
-skip-bind-address
-" >> /etc/mysql/mariadb.conf.d/50-server.cnf
+skip-bind-address" >> /etc/mysql/mariadb.conf.d/50-server.cnf
 
 systemctl restart mariadb
 
-echo "
-CREATE USER '${database_user}'@'${private_bridge_app_ip}' IDENTIFIED BY '${database_pass}';
+echo "CREATE USER '${database_user}'@'${private_bridge_app_ip}' IDENTIFIED BY '${database_pass}';
 CREATE DATABASE IF NOT EXISTS ${database_name} CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 GRANT ALL PRIVILEGES ON ${database_name}.* TO '${database_user}'@'${private_bridge_app_ip}';
-FLUSH PRIVILEGES;
-" > /home/ubuntu/setup.sql
+FLUSH PRIVILEGES;" > /home/ubuntu/setup.sql
 
 mysql < /home/ubuntu/setup.sql
